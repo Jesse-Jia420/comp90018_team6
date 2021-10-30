958b4e7260ac718727aee26b87e11cf9e06419f6
hew4 <hew4@student.unimelb.edu.au>
2021-10-30 13:29:57 +1100

    remove back buttons for fragments

--- Fitness2/app/src/main/java/comp90018/fitness/ui/MainActivity.java
+++ Fitness2/app/src/main/java/comp90018/fitness/ui/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.Moments, R.id.exercise, R.id.map)
+                R.id.Moments, R.id.exercise, R.id.map, R.id.friends, R.id.login)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
